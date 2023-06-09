3586780a81a3f2e227b57e28b27171a02781ed3a
lisaeidloth <lisa.eidloth@gmx.de>
2023-06-09 22:58:20 +0200

    nav hide/show with display utility classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,38 @@
 <template>
-  <v-app>
-    <v-layout>
-      <v-app-bar elevation="2">
-        <template v-slot:prepend>
-          <router-link to="/">
-            <v-icon icon="mdi-periodic-table" to="/" class="ml-1"></v-icon>
-          </router-link>
+  <div>
+    <v-app>
+      <v-layout>
+        <v-app-bar elevation="2">
+          <template v-slot:prepend>
+            <router-link to="/">
+              <v-icon icon="mdi-periodic-table" to="/" class="ml-1"></v-icon>
+            </router-link>
 
-        </template>
+          </template>
 
-        <v-app-bar-title>Threat Modeling Tool Helper</v-app-bar-title>
-      </v-app-bar>
-      <v-navigation-drawer
-          expand-on-hover
-          rail
-          v-if="!mobile"
-      >
-        <v-list nav>
-          <v-list-item v-for="entry in nav" :prepend-icon="entry.icon" :to="entry.target" :title="entry.title" v-show="entry.show()"></v-list-item>
-        </v-list>
+          <v-app-bar-title>Threat Modeling Tool Helper</v-app-bar-title>
+        </v-app-bar>
+        <v-navigation-drawer
+            :expand-on-hover="true"
+            :rail="true"
 
-      </v-navigation-drawer>
-      <v-bottom-navigation v-else>
-        <v-btn v-for="entry in nav" :value="entry.title" :to="entry.target" :icon="entry.icon" v-show="entry.show()"></v-btn>
-      </v-bottom-navigation>
-      <v-main class="ma-3">
-        <slot></slot>
-      </v-main>
-    </v-layout>
+        >
+          <v-list nav>
+            <v-list-item v-for="entry in nav" :prepend-icon="entry.icon" :to="entry.target" :title="entry.title" v-show="entry.show()"></v-list-item>
+          </v-list>
 
-  </v-app>
+        </v-navigation-drawer>
+        <v-bottom-navigation class="d-lg-none">
+          <v-btn v-for="entry in nav" :value="entry.title" :to="entry.target" :icon="entry.icon" v-show="entry.show()"></v-btn>
+        </v-bottom-navigation>
+        <v-main class="ma-3">
+          <slot></slot>
+        </v-main>
+      </v-layout>
+
+    </v-app>
+  </div>
+
 </template>
 
 <script setup lang="ts">
@@ -53,7 +56,6 @@ const nav = reactive([
   },
 ])
 
-const mobile = false //useDisplay().mobile
 </script>
 
 
