9f0624335fc81a69122fb53da3cb232ef339dea7
lisaeidloth <lisa.eidloth@gmx.de>
2023-07-09 19:30:43 +0200

    content update

--- pages/tool/[toolSlug].vue
+++ pages/tool/[toolSlug].vue
@@ -2,10 +2,10 @@
   <div class="ml-4 pa-4">
     <div v-if="!pendingTools && !pendingCritertias">
       <h1 class="mb-2 text-primary">{{ tool.name }}</h1>
-      <div class="pr-5">{{ tool.description }}</div>
+      <div class="pr-5" v-html="tool.description"></div>
       <v-divider class="my-5"></v-divider>
-      <h2>Vor- & Nachteile</h2>
-      <div class="d-flex flex-row" style="gap: 15px">
+      <h2 v-if="showAdvantages">Vor- & Nachteile</h2>
+      <div v-if="showAdvantages" class="d-flex flex-row" style="gap: 15px">
         <div class="d-flex flex-column flex-grow-1" style="flex-basis: 0">
           <h3 class="mt-2 py-2 pl-1 bg-grey-lighten-3">Vorteile</h3>
           <v-list density="compact">
@@ -24,18 +24,18 @@
           <h3 class="mt-2 py-2 pl-1 bg-grey-lighten-3">Nachteile</h3>
           <v-list density="compact">
             <v-list-item
-                v-for="advantages of tool.pros_cons.disadvantages"
+                v-for="disadvantages of tool.pros_cons.disadvantages"
                 class="mb-2 pl-0 bg-grey-lighten-4"
                 prepend-icon="mdi-circle-small"
             >
               <div>
-                <span>{{ advantages }}</span>
+                <span>{{ disadvantages }}</span>
               </div>
             </v-list-item>
           </v-list>
         </div>
       </div>
-      <v-divider class="my-5"></v-divider>
+      <v-divider v-if="showAdvantages" class="my-5"></v-divider>
       <h2>Abgleich Kriterien</h2>
       <v-list class="pr-5">
         <v-list-item
@@ -58,7 +58,7 @@
 
 <script setup lang="ts">
 
-import {useAsyncData} from "#imports";
+import {computed, useAsyncData} from "#imports";
 import {CriteriaEntry, ToolEntry} from "~/utils/types";
 
 const toolSlug = useRoute().params.toolSlug
@@ -73,6 +73,11 @@ const {data: criterias, pending: pendingCritertias} = useAsyncData("criteria-too
     () => queryContent('/criteria').findOne()
         .then(data => data.body as Array<CriteriaEntry>)
 )
+
+
+const showAdvantages = computed(() => {
+  return !pendingTools.value && (tool.value.pros_cons.advantages.length > 0 || tool.value.pros_cons.disadvantages.length > 0)
+})
 
 function isOption(crit: CriteriaEntry): boolean {
   return crit.type === "option"
